fc22d6e19e0f835de29d0e8039153f43b17c14b6
dream <2537687014@qq.com>
2026-06-17 16:57:41 +0800

    1

---
+++ softwares.js
@@ -0,0 +1,137 @@
+﻿const ALL_SOFTWARES ={
+  "products": [
+    {
+      "id": "soft_20260614113912",
+      "name": "植物大战僵尸",
+      "category": "游戏",
+      "weight": "⭐⭐⭐⭐⭐",
+      "mainImage": "logo/logo_20260614175611.png",
+      "detailImages": "logo/logo_20260616164135350.png,logo/logo_20260616164212372.png",
+      "resourcePath": "https://1guo.lanzouw.com/iqkYA1fr80mj",
+      "isRecommend": true,
+      "version": "1.0.0",
+      "size": "52.50MB",
+      "environment": "Win 7 | Win 10 | Win 11",
+      "description": "植物大战僵尸的经典作品，必玩塔防游戏。\r\n极富策略性的小游戏，集成了即时战略、塔防战斗和卡片收集等要素。\r\n在游戏中，玩家通过栽种植物，武装自己的家园，把僵尸阻挡在入侵的道路上，不同的敌人，不同的玩法构成五种不同的游戏模式，加之黑夜、浓雾以及泳池之类的障碍增加了游戏挑战性。"
+    },
+    {
+      "id": "P870",
+      "name": "罪恶都市",
+      "category": "游戏",
+      "weight": "⭐⭐⭐⭐⭐",
+      "mainImage": "logo/logo_20260616231606251.png",
+      "detailImages": "logo/logo_20260616232359267.png,logo/logo_20260616232405150.png",
+      "resourcePath": "",
+      "isRecommend": false,
+      "version": "6.0.0",
+      "size": "1.11GB",
+      "environment": "Win XP | Win 7 | Win 10 | Win 11",
+      "description": "八十年代黑帮横行的罪恶城藏满犯罪机遇，刑满十五年的汤米奉弗雷利家族之命前往当地交易毒品，不料交易遭伏击全盘失败。在家族首领桑尼的要求下，汤米着手追查伏击主谋，并借机在罪恶城发展自身势力。\r\n城市布局由东岛、西岛及虾岛等多个岛屿构成，包含罪城海滩、大洋海滩、市中心、小哈瓦那等风格各异的区域，整体呈现迈阿密与南佛罗里达的都市风貌。\r\n玩家可在庞大的城市地图中自由探索，执行主线与支线任务，从事抢劫、暗杀等活动，并驾驶汽车、摩托车、飞机等多种载具。"
+    },
+    {
+      "id": "P554",
+      "name": "混乱大枪战",
+      "category": "游戏",
+      "weight": "⭐⭐⭐⭐⭐",
+      "mainImage": "logo/logo_20260616170414603.png",
+      "detailImages": "logo/logo_20260616170537288.png,logo/logo_20260616170618600.png",
+      "resourcePath": "",
+      "isRecommend": false,
+      "version": "1.0.0",
+      "size": "5.68 MB",
+      "environment": "Win 7 | Win 10 | Win 11",
+      "description": "混乱大枪战，一款刺激好玩的游戏。\r\n拿起你手中的枪去击败对手吧，小心炸弹哦，可是会把你自己炸飞的。\r\n游戏加载完后点击CAMPAIGN，再选择关卡。\r\n点击选择单人或双人模式，最后点击右下角Start Level即可开始游戏。"
+    },
+    {
+      "id": "P979",
+      "name": "疯狂小人战斗",
+      "category": "游戏",
+      "weight": "⭐⭐⭐⭐⭐",
+      "mainImage": "logo/logo_20260616165015078.png",
+      "detailImages": "logo/logo_20260616164749832.png,logo/logo_20260616164936631.png",
+      "resourcePath": "",
+      "isRecommend": false,
+      "version": "1.0.0",
+      "size": "2.60MB",
+      "environment": "Win 7 | Win 10 | Win 11",
+      "description": "玩家可以选择不同的角色进行对战，包括草莓、桃子、垃圾桶、电球、咖啡豆和茄子等。\r\n每个角色都拥有独特的远程技能、大招和战斗特长。"
+    },
+    {
+      "id": "P936",
+      "name": "勇者之路精灵物语",
+      "category": "游戏",
+      "weight": "⭐⭐⭐⭐⭐",
+      "mainImage": "logo/logo_20260616175200627.png",
+      "detailImages": "logo/logo_20260616175203606.png,logo/logo_20260616175206320.png",
+      "resourcePath": "",
+      "isRecommend": false,
+      "version": "1.0.0",
+      "size": "19.6 MB",
+      "environment": "Win 7 | Win 10 | Win 11",
+      "description": "《勇者之路》系列续作，玩家扮演大陆冒险者。\r\n可以收服功能各异的精灵伙伴，双人配合闯过多层奇幻关卡。\r\n讨伐魔物 BOSS，追寻传说中冒险王遗留的宝藏。"
+    },
+    {
+      "id": "P837",
+      "name": "Snipaste",
+      "category": "工具",
+      "weight": "⭐⭐⭐⭐⭐",
+      "mainImage": "logo/logo_20260617120340043.png",
+      "detailImages": "",
+      "resourcePath": "",
+      "isRecommend": true,
+      "version": "2.8.3",
+      "size": "",
+      "environment": "Win 7 | Win 10 | Win 11",
+      "description": "Snipaste 是一个简单但强大的贴图工具，同时也可以执行截屏、标注等功能。 \r\n快捷键（默认为 F1）、鼠标左键 单击托盘图标。\r\n拥有回放截图记录、逐像素控制光标移动 、 像素级控制截取区域 、放大镜、取色、贴图等功能。"
+    },
+    {
+      "id": "P142",
+      "name": "仙剑奇侠传一",
+      "category": "游戏",
+      "weight": "⭐⭐⭐⭐⭐",
+      "mainImage": "logo/logo_20260617132724115.png",
+      "detailImages": "logo/logo_20260617132726970.png,logo/logo_20260617132731104.png",
+      "resourcePath": "",
+      "isRecommend": false,
+      "version": "1.0.0",
+      "size": "20.7 MB",
+      "environment": "Win 7 | Win 10",
+      "description": "最经典的仙侠游戏大作"
+    },
+    {
+      "id": "P188",
+      "name": "Everything",
+      "category": "工具",
+      "weight": "⭐⭐⭐⭐⭐",
+      "mainImage": "logo/logo_20260617152331946.png",
+      "detailImages": "",
+      "resourcePath": "",
+      "isRecommend": true,
+      "version": "1.4.1",
+      "size": "",
+      "environment": "Win 2000 |Win XP |Win 7 | Win 10 | Win 11",
+      "description": "一款文件搜索工具，基于名称实时定位文件和目录。\r\n体积小巧，界面简洁易用，快速建立索引，快速搜索，同时占用极低的系统资源，实时跟踪文件变化。\r\n在搜索框输入文字，它就会只显示过滤后的文件和目录。\r\nEverything搜索只基于文件和文件夹的名称，创建数据库速度超快。"
+    }
+  ],
+  "announcements": [
+    {
+      "id": "1",
+      "title": "欢迎来到畅享资源大厅",
+      "targetUrl": ""
+    }
+  ],
+  "friendLinks": [
+    {
+      "siteName": "dream",
+      "jumpUrl": "https://1guo.vercel.app/"
+    },
+    {
+      "siteName": "nox",
+      "jumpUrl": "https://yiguo666.github.io/dream/"
+    }
+  ],
+  "categories": [
+    "游戏",
+    "工具"
+  ]
+};
